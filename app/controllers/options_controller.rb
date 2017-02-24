b9341f6dbefed5665eb057ca588acdffa9b4f5e1
appi123dk <appi1234dk@gmail.com>
2017-02-24 13:27:17 +0900

    디테일 오류 수정
    - 주문 시 가격, 수량 오류
    - 주소 수정
    - 뷰 수

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -24,11 +24,22 @@ class OptionsController < ApplicationController
 	end
 
 	def price_edit
-		
+		@price_set = Price.find(params[:id])
 	end
 
 	def price_update
-		
+		price_set = Price.find(params[:id])
+		price_set.price_name = params[:price_name]
+		price_set.price_min = params[:price_min]
+		price_set.price_1 = params[:price_1]
+		price_set.price_2 = params[:price_2]
+		price_set.price_3 = params[:price_3]
+		price_set.price_4 = params[:price_4]
+		price_set.price_5 = params[:price_5]
+		price_set.price_6 = params[:price_6]
+		price_set.save
+
+		redirect_to '/options/index'	
 	end
 
 	def qty_new
@@ -50,10 +61,20 @@ class OptionsController < ApplicationController
 	end
 
 	def qty_edit
-		
+		@qty_set = Quantity.find(params[:id])
 	end
 
 	def qty_update
-		
+		qty_set = Quantity.find(params[:id])
+		qty_set.qty_max = params[:qty_max]
+		qty_set.qty_1 = params[:qty_1]
+		qty_set.qty_2 = params[:qty_2]
+		qty_set.qty_3 = params[:qty_3]
+		qty_set.qty_4 = params[:qty_4]
+		qty_set.qty_5 = params[:qty_5]
+		qty_set.qty_6 = params[:qty_6]
+		qty_set.save
+
+		redirect_to '/options/index'
 	end
 end
